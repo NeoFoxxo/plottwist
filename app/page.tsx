b0ee07bf3385d87f99c47e0d7dc164af8969c3ba
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-04-13 16:19:20 +0300

    shadcn/ui initializtion with mode toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
 import Footer from "@/components/Footer"
 import AuthButton from "../components/AuthButton"
 import Header from "@/components/Header"
+import { ModeToggle } from "@/components/ModeToggle"
 
 export default async function Index() {
 	return (
-		<div className="flex-1 w-full flex flex-col gap-20 items-center">
-			<nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-				<div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+		<div className="flex flex-col items-center flex-1 w-full gap-20">
+			<nav className="flex justify-center w-full h-16 border-b border-b-foreground/10">
+				<div className="flex items-center justify-between w-full max-w-4xl p-3 text-sm">
 					<AuthButton />
+					<ModeToggle />
 				</div>
 			</nav>
 
-			<div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
+			<div className="flex flex-col flex-1 max-w-4xl gap-20 px-3">
 				<Header />
 			</div>
 			<Footer />
