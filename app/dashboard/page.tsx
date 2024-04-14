8724cf44e8fec0b866ad1542592a1fcf8ca47a86
nidhish-nayak <nidhibelthangady@gmail.com>
2024-04-14 11:09:31 +0530

    Dashboard placeholder complete

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,7 @@
-import AuthButton from "@/components/AuthButton";
-import Header from "@/components/Header";
+import AuthButton from "../../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import UserProfile from "../../components/UserProfile";
 
 export default async function ProtectedPage() {
     const supabase = createClient();
@@ -15,12 +15,14 @@ export default async function ProtectedPage() {
     }
 
     return (
-        <div className="flex-1 w-full flex flex-col gap-20 items-center">
-            <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-4xl flex justify-between items-between p-3 text-sm">
+        <div className="flex-1 w-full flex flex-col items-center">
+            <nav className="w-full flex border-b items-center justify-center border-b-foreground/10 h-14">
+                <div className="w-full max-w-6xl flex p-3 items-center justify-between text-sm">
+                    <UserProfile />
                     <AuthButton />
                 </div>
             </nav>
+            <div className="m-4 text-xl text-slate-300">DASHBOARD PLACEHOLDER</div>
         </div>
     );
 }
