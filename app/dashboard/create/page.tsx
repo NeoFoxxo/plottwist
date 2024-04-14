8ccc0f6ecb270a366f18fa1121e78535f5a2c520
Nidhish <76598208+nidhish-nayak@users.noreply.github.com>
2024-04-14 16:34:50 +0530

    Merge branch 'main' into feature/scenarioCards

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -2,17 +2,15 @@ import CreatePrompt from "@/components/CreatePrompt"
 
 export default async function Create() {
 	return (
-		<div className="flex-1 w-full flex flex-col gap-20 items-center">
-			<main className="pt-10 flex-1 text-center flex flex-col gap-3">
-				<h1 className="font-bold text-4xl mb-1">
-					Create your own interactive story
-				</h1>
-				<h2 className="text-1xl mb-5">
-					Write about a scenario and make different choices to affect the
-					stories outcome
-				</h2>
-				<CreatePrompt />
-			</main>
-		</div>
+		<main className="flex flex-col flex-1 gap-3 px-5 pt-10 text-center">
+			<h1 className="mb-1 text-4xl font-bold">
+				Create your own interactive story
+			</h1>
+			<h2 className="mb-5 text-1xl">
+				Write about a scenario and make different choices to affect the
+				stories outcome
+			</h2>
+			<CreatePrompt />
+		</main>
 	)
 }
