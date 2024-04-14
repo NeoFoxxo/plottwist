5bacf64ddad17bfd396d682b36283aa9e51e1ae0
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-04-14 13:41:10 +0300

    add prompt submit functionality

--- components/CreatePrompt.tsx
+++ components/CreatePrompt.tsx
@@ -1,18 +1,68 @@
-import { Textarea } from "@/components/ui/textarea"
-import { submitPrompt } from "@/utils/actions/submitPrompt"
+"use client";
+import { Textarea } from "@/components/ui/textarea";
+import { submitPrompt } from "@/utils/actions/submitPrompt";
+import { Loader2 } from "lucide-react";
 
-export default async function CreatePrompt() {
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { useForm } from "react-hook-form";
+import { Button } from "./ui/button";
+import { useState } from "react";
+
+const createSchema = z.object({
+	prompt: z.string()
+		.min(15, "Minimum characters should be 15!")
+		.max(60, "Maximum characters should be 60!")
+});
+
+export default function CreatePrompt() {
+	const [pending, setPending] = useState(false);
+
+	const form = useForm<z.infer<typeof createSchema>>({
+		resolver: zodResolver(createSchema),
+		defaultValues: {
+			prompt: ""
+		},
+	});
+
+	async function onSubmit(values: z.infer<typeof createSchema>) {
+		setPending(true);
+		try {
+			const result = await submitPrompt(values);
+			console.log(result);
+		} catch (err) {
+			setPending(false);
+		}
+	}
+
 	return (
-		<>
-			<form className="w-full flex flex-col items-center gap-10">
-				<Textarea placeholder="What is your story about?" id="prompt" />
-				<button className="p-[3px] relative" formAction={submitPrompt}>
-					<div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-					<div className="px-4 py-2  bg-black rounded-[6px] relative group transition duration-200 text-white hover:bg-transparent">
-						Create your interactive story
-					</div>
-				</button>
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-4 text-left">
+				<FormField
+					control={form.control}
+					name="prompt"
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Enter your prompt</FormLabel>
+							<FormControl>
+								<Textarea placeholder="What is your story about?" {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				>
+				</FormField>
+				<Button type="submit" disabled={pending}>
+					{pending ?
+						<div className="flex items-center gap-2">
+							<Loader2 className="animate-spin" />
+							Creating your story
+						</div>
+						: "Create your story"
+					}
+				</Button>
 			</form>
-		</>
+		</Form>
 	)
 }
--- utils/actions/submitPrompt.ts
+++ utils/actions/submitPrompt.ts
@@ -1,19 +1,21 @@
-export async function submitPrompt() {
-	"use server"
-	const res = await fetch(`${process.env.AI_API_URL}/start`, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			userid: "e22e2e",
-			prompt: "test",
-		}),
-	})
-	if (!res.ok) {
-		const errorMessage = await res.json()
-		console.log(errorMessage)
-	}
-	const data = await res.json()
-	console.log(data)
+"use server";
+
+export async function submitPrompt({ prompt }: { prompt: string }) {
+    const res = await fetch(`${process.env.AI_API_URL}/start`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            prompt: prompt,
+        }),
+    });
+
+    if (!res.ok) {
+        const errorMessage = await res.json();
+        throw new Error(errorMessage);
+    }
+
+    const data = await res.json();
+    return data.result;
 }
