95d4850b7801e6652d299531b4f2d1a62b6a98ce
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-04-14 13:41:10 +0300

    add prompt submit functionality

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -16,12 +16,6 @@ export default async function ProtectedPage() {
 
     return (
         <div className="flex-1 w-full flex flex-col items-center">
-            <nav className="w-full flex border-b items-center justify-center border-b-foreground/10 h-16">
-                <div className="w-full max-w-7xl flex p-3 items-center justify-between text-sm">
-                    <ProfileButton />
-                    <AuthButton />
-                </div>
-            </nav>
             <div className="m-4 text-xl text-slate-300">DASHBOARD PLACEHOLDER</div>
         </div>
     );
