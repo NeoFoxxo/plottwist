bc4105cd61ea287e26f78f1c1c5f027f9ae0659d
NeoFoxxo <hamish@cits.co.nz>
2024-04-14 17:28:32 +1000

    Created create page and submit prompt server action

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer"
 import AuthButton from "../components/AuthButton"
 import Header from "@/components/Header"
 import { ModeToggle } from "@/components/ModeToggle"
+import Link from "next/link"
 
 export default async function Index() {
 	return (
@@ -15,6 +16,16 @@ export default async function Index() {
 
 			<div className="flex flex-col flex-1 max-w-4xl gap-20 px-3">
 				<Header />
+
+				<Link
+					href="/create"
+					className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+				>
+					<span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+					<span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+						Create your story
+					</span>
+				</Link>
 			</div>
 			<Footer />
 		</div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
 	return (
 		<div className="flex flex-col items-center gap-16">
-			<p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+			<p className="text-cyan-500 lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
 				Plot Twist
 			</p>
 		</div>
