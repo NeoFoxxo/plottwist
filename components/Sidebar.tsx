3b8a61bd5aa01c1d5b48b2c03ab50d6820f6559b
NeoFoxxo <hamish@cits.co.nz>
2024-04-15 22:03:29 +1000

    Redirect user if they access the create page & remove profile from sidebar when not logged in

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,62 +1,72 @@
-import { buttonVariants } from "@/components/ui/button";
-
-import { cn } from "@/lib/utils";
-import Link from "next/link";
-
+import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import Link from "next/link"
 import {
-    BarChartBig,
-    Bookmark,
-    CircleUserRound,
-    LayoutDashboard,
-    SquarePen,
-} from "lucide-react";
+	BarChartBig,
+	Bookmark,
+	CircleUserRound,
+	LayoutDashboard,
+	SquarePen,
+} from "lucide-react"
+import { createClient } from "@/utils/supabase/server"
 
-export function Sidebar() {
-    const links = [
-        {
-            text: "Dashboard",
-            href: "/app",
-            icon: <LayoutDashboard className="w-5 h-5" />,
-        },
-        {
-            text: "Statistics",
-            href: "/app/stats",
-            icon: <BarChartBig className="w-5 h-5" />,
-        },
-        {
-            text: "Bookmarks",
-            href: "/app/bookmarks",
-            icon: <Bookmark className="w-5 h-5" />,
-        },
-        {
-            text: "Create",
-            href: "/app/create",
-            icon: <SquarePen className="w-5 h-5" />,
-        },
-        {
-            text: "Profile",
-            href: "/profile",
-            icon: <CircleUserRound className="w-5 h-5" />,
-        },
-    ];
+export async function Sidebar() {
+	const supabase = createClient()
+	const {
+		data: { user },
+	} = await supabase.auth.getUser()
 
-    return (
-        <div className="sidebar flex-col justify-between min-h-[84vh] max-w-64 md:px-3 bg-black/30 w-80 border-r border-secondary hidden md:flex">
-            <nav className="flex flex-col gap-4 mt-4">
-                {links.map((link) => (
-                    <Link
-                        key={link.text}
-                        href={link.href}
-                        className={cn(
-                            buttonVariants({ variant: "ghost" }),
-                            "flex justify-start items-center gap-3 px-3 py-2 text-base hover:bg-white/20"
-                        )}
-                    >
-                        {link.icon}
-                        <span>{link.text}</span>
-                    </Link>
-                ))}
-            </nav>
-        </div>
-    );
+	const links = [
+		{
+			text: "Dashboard",
+			href: "/app",
+			icon: <LayoutDashboard className="w-5 h-5" />,
+		},
+		{
+			text: "Statistics",
+			href: "/app/stats",
+			icon: <BarChartBig className="w-5 h-5" />,
+		},
+		{
+			text: "Bookmarks",
+			href: "/app/bookmarks",
+			icon: <Bookmark className="w-5 h-5" />,
+		},
+		{
+			text: "Create",
+			href: "/app/create",
+			icon: <SquarePen className="w-5 h-5" />,
+		},
+	]
+
+	const user_id = user?.id
+
+	// if user is authorised add profile to the sidebar
+	if (user_id) {
+		links.push({
+			text: "Profile",
+			href: "/profile",
+			icon: <CircleUserRound className="w-5 h-5" />,
+		})
+	}
+
+	return (
+		<div className="sidebar flex-col justify-between min-h-[84vh] max-w-64 md:px-3 bg-black/30 w-80 border-r border-secondary hidden md:flex">
+			<nav className="flex flex-col gap-4 mt-4">
+				{links.map((link) => (
+					<Link
+						key={link.text}
+						href={link.href}
+						className={cn(
+							buttonVariants({ variant: "ghost" }),
+							"flex justify-start items-center gap-3 px-3 py-2 text-base hover:bg-white/20"
+						)}
+					>
+						{link.icon}
+						<span>{link.text}</span>
+					</Link>
+				))}
+			</nav>
+		</div>
+	)
 }
--- utils/actions/submitPrompt.ts
+++ utils/actions/submitPrompt.ts
@@ -1,21 +1,23 @@
-"use server";
+"use server"
 
 export async function submitPrompt({ prompt }: { prompt: string }) {
-    const res = await fetch(`${process.env.AI_API_URL}/start`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            prompt: prompt,
-        }),
-    });
+	const res = await fetch(`${process.env.AI_API_URL}/start`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			prompt: prompt,
+		}),
+	})
 
-    if (!res.ok) {
-        const errorMessage = await res.json();
-        throw new Error(errorMessage);
-    }
+	if (!res.ok) {
+		const errorMessage = await res.json()
+		throw new Error(errorMessage)
+	}
+	// TODO: insert story and prompt into db
+	const data = await res.json()
 
-    const data = await res.json();
-    return data.result;
+	// TODO: return the id of inserted row
+	return data.result
 }
