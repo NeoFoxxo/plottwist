77b1c16a953b0b097b07b11df1ee02d522666a59
nidhish-nayak <nidhibelthangady@gmail.com>
2024-04-15 15:05:28 +0530

    New profile page added. Data is fetched upon route

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -9,8 +9,28 @@ type PROFILE_DATA_TYPES = {
         name: string | null;
         user_id: string;
     };
+    date: string;
 };
 
-export default function UserProfile({ profileData }: PROFILE_DATA_TYPES) {
-    return <div className="w-full p-5"></div>;
+export default function UserProfile({ profileData, date }: PROFILE_DATA_TYPES) {
+    const { name, image, email, created_at } = profileData;
+    const defaultImage =
+        "https://ik.imagekit.io/fabric01/linkx/default.png?updatedAt=1708353019749";
+
+    return (
+        <div className="flex w-full p-5 flex-col gap-8">
+            <div className="flex gap-4 justify-start items-center">
+                <img
+                    src={image ? image : defaultImage}
+                    alt="default-user"
+                    className="w-16 h-16 rounded-full"
+                />
+                <div>
+                    <h4>{name ? name : "Default Name"}</h4>
+                    <p className="text-[1.15rem] text-gray-300">{email}</p>
+                </div>
+            </div>
+            <p className="text-sm">Created At: {date}</p>
+        </div>
+    );
 }
