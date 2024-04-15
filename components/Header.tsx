6c3be334150ba9fb4741bfc3719b41c7025350b5
bonndevoff <12user12b12@gmail.com>
2024-04-15 01:01:46 -0300

    404 Page build, Added Fonts, Changed Logotype

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 
-import { ModeToggle } from "@/components/ModeToggle";
 import AuthButton from "@/components/AuthButton";
 import ProfileDropdown from "@/components/ProfileDropdown";
 
@@ -10,12 +9,10 @@ export default function Header({ email }: { email: string | undefined }) {
             <div className="w-full max-w-7xl flex p-3 items-center justify-between text-sm">
                 <Link href="/">
                     <div className="text-md font-semibold">
-                        PLOT{" "}
-                        <i className="text-teal-700 dark:text-red-200">TWIST</i>
+                        <p style={{ fontFamily: '"Cabin", sans-serif;' }} className="italic text-xl">plottwist.</p>
                     </div>
                 </Link>
                 <div className="flex gap-5">
-                    <ModeToggle />
                     {email ? <ProfileDropdown email={email} /> : <AuthButton />}
                 </div>
             </div>
