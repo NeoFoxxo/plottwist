3d4f9ef263b99f326e607615140f35c45b16f4f0
nidhish-nayak <nidhibelthangady@gmail.com>
2024-04-16 23:55:41 +0530

    Dashboard fixed - new slider added

--- components/CreatePrompt.tsx
+++ components/CreatePrompt.tsx
@@ -1,170 +1,186 @@
-"use client"
-import { Textarea } from "@/components/ui/textarea"
-import { StoryReturnTypes } from "@/utils/actions/insertStory"
-import { submitPrompt } from "@/utils/actions/submitPrompt"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Bot, Loader2 } from "lucide-react"
-import { useState } from "react"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "./ui/button"
-import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form"
-import { TextGenerateEffect } from "./ui/text-generate-effect"
-import { regenerateStory } from "@/utils/actions/regenerateStory"
+"use client";
+import { Textarea } from "@/components/ui/textarea";
+import { StoryReturnTypes } from "@/utils/actions/insertStory";
+import { regenerateStory } from "@/utils/actions/regenerateStory";
+import { submitPrompt } from "@/utils/actions/submitPrompt";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Bot, Loader2 } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "./ui/button";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 const createSchema = z.object({
-	prompt: z
-		.string()
-		.min(15, "Minimum characters should be 15!")
-		.max(60, "Maximum characters should be 60!"),
-})
+    prompt: z
+        .string()
+        .min(15, "Minimum characters should be 15!")
+        .max(60, "Maximum characters should be 60!"),
+});
 
 export default function CreatePrompt() {
-	const [pending, setPending] = useState(false)
-	const [errorMessage, setErrorMessage] = useState("")
-	const [isDisabled, setIsDisabled] = useState(false)
-	const [isFormOpen, setIsFormOpen] = useState(true)
-	const [scenario, setScenario] = useState<StoryReturnTypes | null>(null)
-	const [prompt, setPrompt] = useState("")
-	const [regenerateCount, setRegenerateCount] = useState(0)
+    const [pending, setPending] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [isDisabled, setIsDisabled] = useState(false);
+    const [isFormOpen, setIsFormOpen] = useState(true);
+    const [scenario, setScenario] = useState<StoryReturnTypes | null>(null);
+    const [prompt, setPrompt] = useState("");
+    const [regenerateCount, setRegenerateCount] = useState(0);
 
-	let attempts = 0
-	let submitErr = ""
+    let attempts = 0;
+    let submitErr = "";
 
-	const form = useForm<z.infer<typeof createSchema>>({
-		resolver: zodResolver(createSchema),
-		defaultValues: {
-			prompt: prompt,
-		},
-	})
+    const form = useForm<z.infer<typeof createSchema>>({
+        resolver: zodResolver(createSchema),
+        defaultValues: {
+            prompt: prompt,
+        },
+    });
 
-	async function onSubmit(values: z.infer<typeof createSchema>) {
-		setPending(true)
-		setErrorMessage("")
+    async function onSubmit(values: z.infer<typeof createSchema>) {
+        setPending(true);
+        setErrorMessage("");
 
-		if (attempts >= 3) {
-			setErrorMessage(`Could not create story: ${submitErr}`)
-			setPending(false)
-			return
-		}
+        if (attempts >= 3) {
+            setErrorMessage(`Could not create story: ${submitErr}`);
+            setPending(false);
+            return;
+        }
 
-		try {
-			const scenarioData = await submitPrompt(values)
-			setPending(false)
-			setIsDisabled(true)
-			setIsFormOpen(false)
-			setScenario(scenarioData)
-			setPrompt(scenarioData.prompt ? scenarioData.prompt : "")
-		} catch (err) {
-			attempts++
-			submitErr = String(err)
-			onSubmit(values)
-		}
-	}
+        try {
+            const scenarioData = await submitPrompt(values);
+            setPending(false);
+            setIsDisabled(true);
+            setIsFormOpen(false);
+            setScenario(scenarioData);
+            setPrompt(scenarioData.prompt ? scenarioData.prompt : "");
+        } catch (err) {
+            attempts++;
+            submitErr = String(err);
+            onSubmit(values);
+        }
+    }
 
-	async function regenerate() {
-		setPending(true)
-		setRegenerateCount((regenerateCount) => regenerateCount + 1)
-		if (regenerateCount >= 3) {
-			setErrorMessage("Maximum regenerate attempts reached")
-			setPending(false)
-			return
-		}
-		const scenarioData = await regenerateStory({
-			prompt: prompt,
-			previousStoryId: scenario?.id!!,
-		})
-		setPending(false)
-		setScenario(scenarioData)
-		setPrompt(scenarioData.prompt ? scenarioData.prompt : "")
-	}
+    async function regenerate() {
+        setPending(true);
+        setRegenerateCount((regenerateCount) => regenerateCount + 1);
+        if (regenerateCount >= 3) {
+            setErrorMessage("Maximum regenerate attempts reached");
+            setPending(false);
+            return;
+        }
+        const scenarioData = await regenerateStory({
+            prompt: prompt,
+            previousStoryId: scenario?.id!!,
+        });
+        setPending(false);
+        setScenario(scenarioData);
+        setPrompt(scenarioData.prompt ? scenarioData.prompt : "");
+    }
 
-	return (
-		<>
-			{isFormOpen && (
-				<Form {...form}>
-					<form
-						onSubmit={form.handleSubmit(onSubmit)}
-						className="w-full p-4 sm:py-4 md:p-0 space-y-6 text-center"
-					>
-						<h1 className="text-4xl font-bold">Create a story.</h1>
-						<FormField
-							control={form.control}
-							name="prompt"
-							render={({ field }) => (
-								<FormItem>
-									<p className="mb-5">
-										Write your own story and make different choices to affect
-										the outcome.
-									</p>
-									<FormControl>
-										<Textarea
-											className="font-mono mx-auto text-center resize-none w-auto sm:w-[35rem]"
-											minLength={15}
-											maxLength={60}
-											placeholder="What is your story about?"
-											{...field}
-										/>
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						></FormField>
-						<Button
-							variant={"outline"}
-							className="w-fit mx-auto"
-							type="submit"
-							disabled={isDisabled}
-						>
-							<div className="flex items-center justify-center gap-2">
-								{pending ? <Loader2 className="animate-spin" /> : <Bot />}
-								Create your story
-							</div>
-						</Button>
-						{errorMessage && (
-							<p style={{ color: "rgba(255,50,105,0.600)" }}>{errorMessage}</p>
-						)}
-					</form>
-				</Form>
-			)}
-			{scenario && (
-				<section className="p-4 flex flex-col flex-wrap justify-center items-start gap-4">
-					<h4 className="text-[1.15rem]">
-						<b>Prompt:</b> {scenario.prompt}
-					</h4>
-					<article className="max-w-[800px] flex flex-col gap-2">
-						<h4 className="font-semibold text-[1.05rem]">Your story:</h4>
-						{/* <div>{scenario.story}</div> */}
-						<TextGenerateEffect
-							className="font-normal text-[1.05rem]"
-							words={scenario.story ? scenario.story : "Loading..."}
-						/>
-					</article>
-					<Button
-						onClick={regenerate}
-						className="font-semibold flex justify-center items-center gap-2"
-					>
-						{pending ? <Loader2 className="animate-spin" /> : <Bot />}
-						Regenerate
-					</Button>
-					<div className="flex flex-col flex-wrap gap-2">
-						<h4 className="font-semibold">Make your choice:</h4>
-						{scenario.choices.map((choice, index) => {
-							return (
-								<div
-									key={index}
-									className="py-2 px-4 cursor-pointer bg-neutral-800 hover:bg-neutral-900 w-fit rounded-md"
-								>
-									{choice}
-								</div>
-							)
-						})}
-						{errorMessage && (
-							<p style={{ color: "rgba(255,50,105,0.600)" }}>{errorMessage}</p>
-						)}
-					</div>
-				</section>
-			)}
-		</>
-	)
+    return (
+        <>
+            {isFormOpen && (
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="w-full p-4 sm:py-4 md:p-0 space-y-6 text-center"
+                    >
+                        <h1 className="text-4xl font-bold">Create a story.</h1>
+                        <FormField
+                            control={form.control}
+                            name="prompt"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <p className="mb-5">
+                                        Write your own story and make different
+                                        choices to affect the outcome.
+                                    </p>
+                                    <FormControl>
+                                        <Textarea
+                                            className="font-mono mx-auto text-center resize-none w-auto sm:w-[35rem]"
+                                            minLength={15}
+                                            maxLength={60}
+                                            placeholder="What is your story about?"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        ></FormField>
+                        <Button
+                            variant="default"
+                            className="w-fit mx-auto"
+                            type="submit"
+                            disabled={isDisabled}
+                        >
+                            <div className="flex items-center justify-center gap-2">
+                                {pending ? (
+                                    <Loader2 className="animate-spin" />
+                                ) : (
+                                    <Bot />
+                                )}
+                                Create your story
+                            </div>
+                        </Button>
+                        {errorMessage && (
+                            <p style={{ color: "rgba(255,50,105,0.600)" }}>
+                                {errorMessage}
+                            </p>
+                        )}
+                    </form>
+                </Form>
+            )}
+            {scenario && (
+                <section className="p-4 flex flex-col flex-wrap justify-center items-start gap-4">
+                    <h4 className="text-[1.15rem]">
+                        <b>Prompt:</b> {scenario.prompt}
+                    </h4>
+                    <article className="max-w-[800px] flex flex-col gap-2">
+                        <h4 className="font-semibold text-[1.05rem]">
+                            Your story:
+                        </h4>
+                        {/* <div>{scenario.story}</div> */}
+                        <TextGenerateEffect
+                            className="font-normal text-[1.05rem]"
+                            words={
+                                scenario.story ? scenario.story : "Loading..."
+                            }
+                        />
+                    </article>
+                    <Button
+                        onClick={regenerate}
+                        className="font-semibold flex justify-center items-center gap-2"
+                    >
+                        {pending ? (
+                            <Loader2 className="animate-spin" />
+                        ) : (
+                            <Bot />
+                        )}
+                        Regenerate
+                    </Button>
+                    <div className="flex flex-col flex-wrap gap-2">
+                        <h4 className="font-semibold">Make your choice:</h4>
+                        {scenario.choices.map((choice, index) => {
+                            return (
+                                <div
+                                    key={index}
+                                    className="py-2 px-4 cursor-pointer bg-neutral-800 hover:bg-neutral-900 w-fit rounded-md"
+                                >
+                                    {choice}
+                                </div>
+                            );
+                        })}
+                        {errorMessage && (
+                            <p style={{ color: "rgba(255,50,105,0.600)" }}>
+                                {errorMessage}
+                            </p>
+                        )}
+                    </div>
+                </section>
+            )}
+        </>
+    );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,69 +1,61 @@
-import { buttonVariants } from "@/components/ui/button"
-import { cn } from "@/lib/utils"
-import Link from "next/link"
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import {
-	BarChartBig,
-	BookText,
-	Bookmark,
-	LayoutDashboard,
-	LibraryBig,
-	SquareLibrary,
-	SquarePen,
-} from "lucide-react"
+    BarChartBig,
+    Bookmark,
+    LayoutDashboard,
+    LibraryBig,
+    SquarePen,
+} from "lucide-react";
+import Link from "next/link";
 
 export function Sidebar() {
-	const links = [
-		{
-			text: "Dashboard",
-			href: "/app",
-			icon: <LayoutDashboard className="w-5 h-5" />,
-		},
-		{
-			text: "Statistics",
-			href: "/app/stats",
-			icon: <BarChartBig className="w-5 h-5" />,
-		},
-		{
-			text: "My Library",
-			href: "/app/library",
-			icon: <LibraryBig className="w-5 h-5" />,
-		},
-		{
-			text: "Bookmarks",
-			href: "/app/bookmarks",
-			icon: <Bookmark className="w-5 h-5" />,
-		},
-		{
-			text: "Create",
-			href: "/app/create",
-			icon: <SquarePen className="w-5 h-5" />,
-		},
-	]
+    const links = [
+        {
+            text: "Dashboard",
+            href: "/app",
+            icon: <LayoutDashboard className="w-5 h-5" />,
+        },
+        {
+            text: "Statistics",
+            href: "/app/stats",
+            icon: <BarChartBig className="w-5 h-5" />,
+        },
+        {
+            text: "My Library",
+            href: "/app/library",
+            icon: <LibraryBig className="w-5 h-5" />,
+        },
+        {
+            text: "Bookmarks",
+            href: "/app/bookmarks",
+            icon: <Bookmark className="w-5 h-5" />,
+        },
+        {
+            text: "Create",
+            href: "/app/create",
+            icon: <SquarePen className="w-5 h-5" />,
+        },
+    ];
 
-	return (
-		<div
-			style={{
-				height: "calc(99vh - 110px)",
-				overflowY: "auto",
-				borderRight: "solid 1px rgba(255,255,255,0.150)",
-			}}
-			className="sidebar flex flex-col justify-between min-h-20 px-3 bg-black/30 w-80 max-lg:hidden"
-		>
-			<nav className="flex flex-col gap-4 mt-4">
-				{links.map((link) => (
-					<Link
-						href={link.href}
-						className={cn(
-							buttonVariants({ variant: "ghost" }),
-							"flex justify-start items-center gap-3 px-3 py-2 text-base hover:bg-white/20"
-						)}
-					>
-						{link.icon}
-						{link.text}
-					</Link>
-				))}
-			</nav>
-			{/* <UpgradeCard /> */}
-		</div>
-	)
+    return (
+        <div className="group transition-all ease-in-out delay-150 w-[70px] hover:w-80 bg-zinc-950 z-50 fixed top-16 left-0 flex flex-col justify-between h-[100vh] px-3 max-lg:hidden border-r border-[rgba(255,255,255,0.15)] duration-300">
+            <nav className="flex flex-col gap-4 mt-4">
+                {links.map((link) => (
+                    <Link
+                        href={link.href}
+                        className={cn(
+                            buttonVariants({ variant: "ghost" }),
+                            "flex justify-start items-center gap-3 px-3 py-2 text-base hover:bg-secondary overflow-hidden"
+                        )}
+                    >
+                        <span className="block">{link.icon}</span>
+                        <span className="hidden opacity-0 group-hover:block group-hover:opacity-100">
+                            {link.text}
+                        </span>
+                    </Link>
+                ))}
+            </nav>
+        </div>
+    );
 }
