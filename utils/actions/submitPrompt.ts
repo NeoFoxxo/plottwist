d20fa048bad3d32b3549ad6ac8b463692df0bc5e
NeoFoxxo <hamish@cits.co.nz>
2024-04-16 20:20:32 +1000

    user can now not regenerate a story more than 3 times & previous story is deleted if regenerate is used

--- utils/actions/submitPrompt.ts
+++ utils/actions/submitPrompt.ts
@@ -1,49 +1,64 @@
-"use server";
-import { AIResponse } from "../supabase/types/AIResponse";
-import insertStory from "./insertStory";
+"use server"
+import { AIResponse } from "../supabase/types/AIResponse"
+import deleteStory from "./deleteStory"
+import insertStory from "./insertStory"
 
-export async function submitPrompt({ prompt }: { prompt: string }) {
-    const res = await fetch(`${process.env.AI_API_URL}/start`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            prompt: prompt,
-        }),
-    });
+export async function submitPrompt({
+	prompt,
+	isRegenerate,
+	previousStoryId,
+}: {
+	prompt: string
+	isRegenerate?: boolean
+	previousStoryId?: number
+}) {
+	const res = await fetch(`${process.env.AI_API_URL}/start`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			prompt: prompt,
+		}),
+	})
 
-    if (!res.ok) {
-        const errorMessage = await res.json();
-        throw new Error(errorMessage);
-    }
+	if (!res.ok) {
+		const errorMessage = await res.json()
+		throw new Error(errorMessage)
+	}
 
-    const data = await res.json();
-    const aiResponse: AIResponse = data.result.Output;
+	const data = await res.json()
+	const aiResponse: AIResponse = data.result.Output
 
-    const story = aiResponse.response.story;
-    if (!story) {
-        throw new Error("Story data is undefined, please try again");
-    }
+	const story = aiResponse.response.story
+	if (!story) {
+		throw new Error("Story data is undefined, please try again")
+	}
 
-    let choices: string[];
-    try {
-        choices = aiResponse.choices.choices.map(
-            (singleChoice) => singleChoice
-        );
-    } catch (error) {
-        throw new Error("Choice data is undefined, please try again");
-    }
+	let choices: string[]
+	try {
+		choices = aiResponse.choices.choices.map((singleChoice) => singleChoice)
+	} catch (error) {
+		throw new Error("Choice data is undefined, please try again")
+	}
 
-    const title = aiResponse.title.title;
-    if (!title) {
-        throw new Error("Title is undefined, please try again");
-    }
+	const title = aiResponse.title.title
+	if (!title) {
+		throw new Error("Title is undefined, please try again")
+	}
 
-    try {
-        const scenario = insertStory({ title, story, choices, prompt });
-        return scenario;
-    } catch (error) {
-        throw new Error(String(error));
-    }
+	if (isRegenerate && previousStoryId) {
+		try {
+			await deleteStory(previousStoryId)
+		} catch (error) {
+			throw new Error(String(error))
+		}
+	}
+
+	try {
+		const scenario = insertStory({ title, story, choices, prompt })
+		return scenario
+	} catch (error) {
+		throw new Error(String(error))
+	}
 }
