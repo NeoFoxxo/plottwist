c93d7ce23e8fedefb718e278765db7c5fe6b8ea9
bonndevoff <12user12b12@gmail.com>
2024-04-17 03:25:51 -0300

    updated: Profile info is now pulled from db
    added Description for profile creation

--- utils/actions/getUserinfo.ts
+++ utils/actions/getUserinfo.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@/utils/supabase/server"
+import { createClient } from "@/utils/supabase/client"
 
 export default async function getUserInfo(user_id: string) {
     const supabase = createClient()
