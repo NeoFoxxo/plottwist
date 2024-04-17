bdcce7203186b5a0b02e6593a2dc4df484169b46
NeoFoxxo <hamish@cits.co.nz>
2024-04-17 17:22:52 +1000

    Fixed createProfile function link

--- app/app/stats/page.tsx
+++ app/app/stats/page.tsx
@@ -1,19 +1,18 @@
-import CreatePrompt from "@/components/CreatePrompt"
 import { createClient } from "@/utils/supabase/server"
 import { redirect } from "next/navigation"
 
 export default async function Stats() {
-    const supabase = createClient()
-    const {
-        data: { user },
-    } = await supabase.auth.getUser()
+	const supabase = createClient()
+	const {
+		data: { user },
+	} = await supabase.auth.getUser()
 
-    const user_id = user?.id
-    if (!user_id) redirect("/login")
+	const user_id = user?.id
+	if (!user_id) redirect("/login")
 
-    return (
-        <main className="flex flex-col flex-1 my-4 md:p-10 justify-start items-center w-full md:w-fit">
-
-        </main>
-    )
+	return (
+		<main className="flex flex-col flex-1 my-4 md:p-10 justify-start items-center w-full md:w-fit">
+			Stats Page
+		</main>
+	)
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,71 +1,66 @@
-import { siteurl } from "@/utils/siteurl";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
-import { SubmitButton } from "./submit-button";
+import { siteurl } from "@/utils/siteurl"
+import { createClient } from "@/utils/supabase/server"
+import { redirect } from "next/navigation"
+import { SubmitButton } from "./submit-button"
 
 export default function Login({
-    searchParams,
+	searchParams,
 }: {
-    searchParams: { message: string };
+	searchParams: { message: string }
 }) {
-    async function signIn(formData: FormData) {
-        "use server";
+	async function signIn(formData: FormData) {
+		"use server"
 
-        const email = formData.get("email") as string;
-        const supabase = createClient();
+		const email = formData.get("email") as string
+		const supabase = createClient()
 
-        const { data, error } = await supabase.auth.signInWithOtp({
-            email: email,
-            options: {
-                emailRedirectTo: `${siteurl}/api/auth/callback`,
-            },
-        });
+		const { data, error } = await supabase.auth.signInWithOtp({
+			email: email,
+			options: {
+				emailRedirectTo: `${siteurl}/api/auth/callback`,
+			},
+		})
 
-        if (error) {
-            console.log(error.message);
-            return redirect(
-                `/login?message=Could not authenticate user: ${error.message}`
-            );
-        }
+		if (error) {
+			return redirect(
+				`/login?message=Could not authenticate user: ${error.message}`
+			)
+		}
 
-        //here we should insert the username (userName)
+		return redirect("/login?message=Check your email to sign in to Plot Twist!")
+	}
 
-        return redirect(
-            "/login?message=Check your email to sign in to Plot Twist!"
-        );
-    }
-
-    return (
-        <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-            <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-                <div className="mb-5">
-                    <h1 className="text-2xl font-bold">Sign in</h1>
-                    <h2>Simply provide your email to continue to Plot Twist</h2>
-                </div>
-                <label className="text-md" htmlFor="email">
-                    Email
-                </label>
-                <input
-                    className="rounded-md px-4 py-2 bg-inherit border mb-6"
-                    name="email"
-                    type="email"
-                    placeholder="you@example.com"
-                    required
-                />
-                <SubmitButton
-                    formAction={signIn}
-                    style={{ border: 'solid 1px rgba(255,255,255,0.300)' }}
-                    className="bg-black/30 hover:bg-gray-400/30 transition-all rounded-md px-4 py-2 text-foreground mb-2"
-                    pendingText="Signing In..."
-                >
-                    Sign In
-                </SubmitButton>
-                {searchParams?.message && (
-                    <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-                        {searchParams.message}
-                    </p>
-                )}
-            </form>
-        </div>
-    );
+	return (
+		<div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+			<form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+				<div className="mb-5">
+					<h1 className="text-2xl font-bold">Sign in</h1>
+					<h2>Simply provide your email to continue to Plot Twist</h2>
+				</div>
+				<label className="text-md" htmlFor="email">
+					Email
+				</label>
+				<input
+					className="rounded-md px-4 py-2 bg-inherit border mb-6"
+					name="email"
+					type="email"
+					placeholder="you@example.com"
+					required
+				/>
+				<SubmitButton
+					formAction={signIn}
+					style={{ border: "solid 1px rgba(255,255,255,0.300)" }}
+					className="bg-black/30 hover:bg-gray-400/30 transition-all rounded-md px-4 py-2 text-foreground mb-2"
+					pendingText="Signing In..."
+				>
+					Sign In
+				</SubmitButton>
+				{searchParams?.message && (
+					<p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+						{searchParams.message}
+					</p>
+				)}
+			</form>
+		</div>
+	)
 }
