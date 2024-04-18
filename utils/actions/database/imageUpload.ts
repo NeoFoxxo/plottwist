b82d7a92664c2828e84b7e41c87b27c233062ecb
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-04-18 19:35:40 +0300

    final commit for the profile issue

--- utils/actions/database/imageUpload.ts
+++ utils/actions/database/imageUpload.ts
@@ -1,39 +1,19 @@
-import { createClient } from "@/utils/supabase/client"
+import { createClient } from "@/utils/supabase/client";
 
-export default async function imageUpload(
-	file: File,
-	user_id: string,
-	existingImage: boolean
-) {
-	console.log(user_id)
-	const supabase = createClient()
-	const pathPrefix = process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL
+export default async function imageUpload(file: File) {
+    const supabase = createClient();
 
-	// delete existing image if it exists
-	if (existingImage) {
-		const { data: currentImage, error } = await supabase.storage
-			.from("avatars")
-			.list(user_id)
+    const fileNamePrefix: string = Date.now().toString();
+    const fileName: string = fileNamePrefix + file.name;
 
-		if (error) throw new Error("Could find existingImage: ", error)
+    const pathPrefix = process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL;
 
-		// yes we could use update here but it would require a page refresh due to the image cache
-		const { error: deleteErr } = await supabase.storage
-			.from("avatars")
-			.remove([user_id + "/" + currentImage!![0].name])
-		if (deleteErr)
-			throw new Error("Could not delete profile Image: ", deleteErr)
-	}
+    if (!pathPrefix) throw Error(".env for Storage URL not found!");
 
-	const fileNamePrefix: string = Date.now().toString()
-	const fileName: string = user_id + "/" + `profile_${fileNamePrefix}`
+    const { data, error } = await supabase.storage
+        .from("avatars")
+        .upload(fileName, file);
 
-	if (!pathPrefix) throw Error(".env for Storage URL not found!")
-
-	const { data, error } = await supabase.storage
-		.from("avatars")
-		.upload(fileName, file)
-
-	if (error) throw new Error("Could upload image: ", error)
-	return pathPrefix + "/" + data.path
+    if (error) return false;
+    return pathPrefix + "/" + data.path;
 }
