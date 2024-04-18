d3a6b940f6a6a73ef2c8b01a941c745ed2d270fd
NeoFoxxo <hamish@cits.co.nz>
2024-04-18 11:05:08 +1000

    remove react query devtools

--- lib/ReactQueryProvider.tsx
+++ lib/ReactQueryProvider.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 export default function ReactQueryProvider({
 	children,
@@ -10,9 +9,6 @@ export default function ReactQueryProvider({
 	const queryClient = new QueryClient()
 
 	return (
-		<QueryClientProvider client={queryClient}>
-			{children}
-			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
+		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 	)
 }
