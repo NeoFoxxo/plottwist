0adc61318a8cee9fb58d309abee89461497acc20
nidhish-nayak <nidhibelthangady@gmail.com>
2024-04-19 01:35:18 +0530

    All continue features working - *Bug*(story data doesnot refresh on response)

--- components/ContinueStory.tsx
+++ components/ContinueStory.tsx
@@ -1,22 +1,18 @@
-"use client"
-import { Textarea } from "@/components/ui/textarea"
-import { continueStory } from "@/utils/actions/api/continueStory"
-import { StoryReturnTypes } from "@/utils/actions/database/insertStory"
-import { regenerateStory } from "@/utils/actions/api/regenerateStory"
-import { submitPrompt } from "@/utils/actions/api/submitPrompt"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Bot, Loader2 } from "lucide-react"
-import { useState } from "react"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "./ui/button"
-import { TextGenerateEffect } from "./ui/text-generate-effect"
-import { finishStory } from "@/utils/actions/api/finishStory"
-import Link from "next/link"
-import { ChildProcess } from "child_process"
+"use client";
 
-export default function ContinueStory({ story }: {
-    story: {
+import { StoryReturnTypes } from "@/utils/actions/database/insertStory";
+import { useContinuePrompt } from "@/utils/mutations/useContinuePrompt";
+import { useFinishPrompt } from "@/utils/mutations/useFinishPrompt";
+import { useRegeneratePrompt } from "@/utils/mutations/useRegeneratePrompt";
+import { Bot, Loader2 } from "lucide-react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { z } from "zod";
+import { Button } from "./ui/button";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
+
+type CONTINUE_STORY_TYPES = {
+    scenarioData: {
         choices: string[] | null;
         created_at: string;
         finished: boolean | null;
@@ -27,101 +23,157 @@ export default function ContinueStory({ story }: {
         story: string | null;
         title: string;
         user_id: string;
-    } | null
-}
-) {
-    const [pending, setPending] = useState(false)
-    const [errorMessage, setErrorMessage] = useState("")
-    const [scenario, setScenario] = useState(story)
-    const [storyPartCount, setStoryPartCount] = useState(0)
+    };
+};
+
+const createSchema = z.object({
+    prompt: z
+        .string()
+        .min(15, "Minimum characters should be 15!")
+        .max(60, "Maximum characters should be 60!"),
+});
+
+export default function ContinueStory({ scenarioData }: CONTINUE_STORY_TYPES) {
+    const [pending, setPending] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [scenario, setScenario] = useState<StoryReturnTypes | null>(
+        scenarioData
+    );
+    const [prompt, setPrompt] = useState(
+        scenarioData.prompt ? scenarioData.prompt : ""
+    );
+    const [regenerateCount, setRegenerateCount] = useState(0);
+    const [storyPartCount, setStoryPartCount] = useState(0);
+
+    const regeneratePromptRequest = useRegeneratePrompt({
+        setScenario,
+        setErrorMessage,
+    });
+
+    const continueRequest = useContinuePrompt({
+        setScenario,
+        setErrorMessage,
+    });
+
+    const finishRequest = useFinishPrompt({
+        setScenario,
+        setErrorMessage,
+    });
+
+    const isLoading =
+        regeneratePromptRequest.isPending ||
+        continueRequest.isPending ||
+        finishRequest.isPending;
+
+    useEffect(() => {
+        setPending(isLoading);
+    }, [isLoading]);
+
+    async function regenerate() {
+        setRegenerateCount((regenerateCount) => regenerateCount + 1);
+        setErrorMessage("");
+        if (regenerateCount >= 3) {
+            setErrorMessage("Maximum regenerate attempts reached");
+            setPending(false);
+            return;
+        }
+        regeneratePromptRequest.mutate({
+            prompt: prompt,
+            previousStoryId: scenario?.id!!,
+        });
+    }
 
     async function generateFromChoice(choice: string) {
-        console.log(choice)
-        setPending(true)
-        setStoryPartCount((storyPartCount) => storyPartCount + 1)
-
-        let scenarioData: StoryReturnTypes
-
+        setStoryPartCount((storyPartCount) => storyPartCount + 1);
+        setErrorMessage("");
         if (storyPartCount >= 8) {
-            scenarioData = await finishStory({
+            finishRequest.mutate({
                 title: scenario?.title!,
                 prompt: `${scenario?.story!!} ${choice}`,
                 previousStoryId: scenario?.id!!,
                 currentStory: scenario?.story!!,
-            })
+            });
         } else {
-            scenarioData = await continueStory({
+            continueRequest.mutate({
                 title: scenario?.title!,
                 prompt: `${scenario?.story!!} ${choice}`,
                 previousStoryId: scenario?.id!!,
                 currentStory: scenario?.story!!,
-            })
+            });
         }
-
-        setPending(false)
-        setScenario(story)
     }
 
     return (
-        <>
-            {scenario && (
-                <section className="p-4 flex flex-col flex-wrap justify-center items-start gap-4 max-w-[800px]">
-                    <h1 className="text-4xl font-bold pb-10">{scenario.title}</h1>
-                    <h4 className="text-[1.15rem]">
-                        <b>Prompt:</b> {scenario.prompt}
-                    </h4>
-                    <article className="flex flex-col gap-2">
-                        <h4 className="font-semibold text-[1.05rem]">Your story:</h4>
-                        <TextGenerateEffect
-                            className="font-normal text-[1.05rem]"
-                            words={scenario.story ? scenario.story : "Loading..."}
-                        />
-                    </article>
-                    {
-                        pending && (
-                            <h5 className="font-semibold flex justify-center items-center gap-2">
-                                Selecting Choice <Loader2 className="animate-spin" />
-                            </h5>
-                        )
+        <section className="p-4 flex flex-col flex-wrap justify-center items-start gap-4 max-w-[800px]">
+            <h1 className="pb-10 text-4xl font-bold">{scenarioData.title}</h1>
+            <h4 className="text-[1.15rem]">
+                <b>Prompt:</b> {scenarioData.prompt}
+            </h4>
+            <article className="flex flex-col gap-2">
+                <h4 className="font-semibold text-[1.05rem]">Your story:</h4>
+                <TextGenerateEffect
+                    className="font-normal text-[1.05rem]"
+                    words={
+                        scenarioData.story ? scenarioData.story : "Loading..."
                     }
-                    <div className="flex flex-col flex-wrap gap-2">
-                        {storyPartCount >= 9 ? (
-                            <>
-                                {!pending && (
-                                    <>
-                                        <h4 className="text-lg font-bold">
-                                            Your story is finished!
-                                        </h4>
-                                        <Link href={"/app/library"}>
-                                            <Button className="font-semibold flex justify-center items-center gap-2">
-                                                Go to Library
-                                            </Button>
-                                        </Link>
-                                    </>
-                                )}
-                            </>
-                        ) : (
-                            <>
-                                <h4 className="font-semibold">Make your choice:</h4>
-                                {scenario!!.choices!!.map((choice, index) => {
-                                    return (
-                                        <div
-                                            key={index}
-                                            onClick={async () => await generateFromChoice(choice)}
-                                            className="py-2 px-4 cursor-pointer bg-neutral-800 hover:bg-neutral-900 w-fit rounded-md"
-                                        >
-                                            {choice}
-                                        </div>
-                                    )
-                                })}
-                            </>
-                        )}
-                        {errorMessage && (
-                            <p style={{ color: "rgba(255,50,105,0.600)" }}>{errorMessage}</p>
-                        )}
-                    </div>
-                </section>
+                />
+            </article>
+
+            {storyPartCount === 0 ? (
+                <Button
+                    onClick={regenerate}
+                    className="flex items-center justify-center gap-2 font-semibold"
+                >
+                    {pending ? <Loader2 className="animate-spin" /> : <Bot />}
+                    Regenerate
+                </Button>
+            ) : (
+                pending && (
+                    <h5 className="flex items-center justify-center gap-2 font-semibold">
+                        Selecting Choice <Loader2 className="animate-spin" />
+                    </h5>
+                )
             )}
-        </>
-    )
+            <div className="flex flex-col flex-wrap gap-2">
+                {storyPartCount >= 9 ? (
+                    <>
+                        {!pending && (
+                            <>
+                                <h4 className="text-lg font-bold">
+                                    Your story is finished!
+                                </h4>
+                                <Link href={"/app/library"}>
+                                    <Button className="flex items-center justify-center gap-2 font-semibold">
+                                        Go to Library
+                                    </Button>
+                                </Link>
+                            </>
+                        )}
+                    </>
+                ) : (
+                    <>
+                        <h4 className="font-semibold">Make your choice:</h4>
+                        {scenario!!.choices!!.map((choice, index) => {
+                            return (
+                                <div
+                                    key={index}
+                                    onClick={async () =>
+                                        await generateFromChoice(choice)
+                                    }
+                                    className="px-4 py-2 rounded-md cursor-pointer bg-neutral-800 hover:bg-neutral-900 w-fit"
+                                >
+                                    {choice}
+                                </div>
+                            );
+                        })}
+                    </>
+                )}
+                {errorMessage && (
+                    <p style={{ color: "rgba(255,50,105,0.600)" }}>
+                        {errorMessage}
+                    </p>
+                )}
+            </div>
+        </section>
+    );
 }
--- components/LibraryCard.tsx
+++ components/LibraryCard.tsx
@@ -1,190 +1,193 @@
-"use client"
-import { Lock, Globe, Loader2 } from "lucide-react"
-import { CardBody, CardContainer, CardItem } from "./ui/3d-card"
+"use client";
 import {
-	Tooltip,
-	TooltipContent,
-	TooltipProvider,
-	TooltipTrigger,
-} from "@/components/ui/tooltip"
-import publish from "@/utils/actions/database/publishStory"
-import unPublish from "@/utils/actions/database/privateStory"
-import { useRouter } from "next/navigation"
-import { useState } from "react"
-import { continueStory } from "@/utils/actions/api/continueStory"
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
+import unPublish from "@/utils/actions/database/privateStory";
+import publish from "@/utils/actions/database/publishStory";
+import { Globe, Loader2, Lock } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 
 type SCENARIO_TYPES = {
-	scenario: {
-		created_at: string
-		follow_count: number
-		id: number
-		prompt: string | null
-		story: string | null
-		title: string
-		user_id: string
-		finished: boolean | null
-		published: boolean | null
-	}
-}
+    scenario: {
+        created_at: string;
+        follow_count: number;
+        id: number;
+        prompt: string | null;
+        story: string | null;
+        title: string;
+        user_id: string;
+        finished: boolean | null;
+        published: boolean | null;
+    };
+};
 
 function truncateString(str: string, maxl: number) {
-	// Trim any leading or trailing spaces
-	str?.trim()
+    // Trim any leading or trailing spaces
+    str?.trim();
 
-	if (str?.length > maxl) {
-		// Find the index of the last space before the 120th character
-		let lastSpaceIndex = str.lastIndexOf(" ", maxl)
-		// If no space is found, truncate at 120th character
-		let endIndex = lastSpaceIndex === -1 ? maxl : lastSpaceIndex
+    if (str?.length > maxl) {
+        // Find the index of the last space before the 120th character
+        let lastSpaceIndex = str.lastIndexOf(" ", maxl);
+        // If no space is found, truncate at 120th character
+        let endIndex = lastSpaceIndex === -1 ? maxl : lastSpaceIndex;
 
-		return str.substring(0, endIndex).trim() + " ..."
-	} else {
-		return str
-	}
+        return str.substring(0, endIndex).trim() + " ...";
+    } else {
+        return str;
+    }
 }
 
 const shadowcolor = [
-	"hover:shadow-emerald-500/[0.4]",
-	"hover:shadow-orange-400/[0.6]",
-	"hover:shadow-blue-600/[0.6]",
-	"hover:shadow-cyan-400/[0.6]",
-	"hover:shadow-violet-500/[0.4]",
-	"hover:shadow-red-300/[0.4]",
-]
+    "hover:shadow-emerald-500/[0.4]",
+    "hover:shadow-orange-400/[0.6]",
+    "hover:shadow-blue-600/[0.6]",
+    "hover:shadow-cyan-400/[0.6]",
+    "hover:shadow-violet-500/[0.4]",
+    "hover:shadow-red-300/[0.4]",
+];
 
 const bordercolor = [
-	"border-emerald-500/[0.6]",
-	"border-orange-500/[0.6]",
-	"border-blue-500/[0.6]",
-	"border-cyan-500/[0.6]",
-	"border-violet-500/[0.6]",
-	"border-red-300/[0.6]",
-]
+    "border-emerald-500/[0.6]",
+    "border-orange-500/[0.6]",
+    "border-blue-500/[0.6]",
+    "border-cyan-500/[0.6]",
+    "border-violet-500/[0.6]",
+    "border-red-300/[0.6]",
+];
 
 export function LibraryCard({ scenario }: SCENARIO_TYPES) {
-	const router = useRouter()
+    const router = useRouter();
 
-	const [pending, setPending] = useState(false)
+    const [pending, setPending] = useState(false);
 
-	const r = Math.floor(Math.random() * shadowcolor.length)
+    const r = Math.floor(Math.random() * shadowcolor.length);
 
-	const { title, prompt, story, finished, published } = scenario
+    const { title, prompt, story, finished, published } = scenario;
 
-	return (
-		<CardContainer className="inter-var h-[10rem] p-0 my-7">
-			<CardBody
-				className={`transition-all bg-gray-50 relative group/card shadow-2xl dark:bg-black/50 ${bordercolor[r]} ${shadowcolor[r]} hover:border-white w-auto h-auto max-md:h-auto my-auto sm:w-[25rem] max-w-[25rem] rounded-xl p-7 m-10 border flex flex-col`}
-			>
-				<CardItem
-					href={`/story/${scenario.id}`}
-					as="a"
-					style={{ cursor: "pointer" }}
-					translateZ="50"
-					className="text-xl font-bold text-neutral-600 dark:text-white"
-				>
-					{title}
-				</CardItem>
-				<CardItem
-					as="p"
-					translateZ="60"
-					className="text-neutral-400 text-xs max-w-sm mt-2 dark:text-neutral-400/80 hover:dark:text-neutral-400/100"
-				>
-					{truncateString(story!!, 180)}
-				</CardItem>
-				<CardItem
-					as="p"
-					translateZ={60}
-					style={{
-						backgroundColor: "rgba(0,0,0,0.700)",
-						borderRadius: "1em",
-						lineHeight: "1rem",
-						overflowWrap: "break-word",
-					}}
-					className="text-neutral-600 p-2 transition-all duration-1000 ease-in-out font-mono text-[0.6rem] hover:text-[0.8rem] w-[100%] my-4 dark:text-neutral-500 hover:dark:text-neutral-300"
-				>
-					Prompt: {prompt}
-				</CardItem>
-				<div className="flex justify-between items-center mt-auto">
-					{finished == false && (
-						<CardItem
-							as={'a'}
-							href={`/app/continue/${scenario.id}`}
-							translateZ={74}
-							style={{ borderRadius: "1em" }}
-							className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white bg-transparent hover:bg-white/20 "
-						>
-							Continue →
-						</CardItem>
-					)}
-					<CardItem translateZ={70}>
-						<div className="flex justify-end mt-auto">
-							{finished == true && (
-								<>
-									{published ? (
-										<CardItem
-											onClick={async () => {
-												setPending(true)
-												await unPublish(scenario.id)
-												router.refresh()
-												setPending(false)
-											}}
-											style={{ borderRadius: "1em" }}
-											className="mr-2 rounded-x bg-transparent hover:bg-white/20 text-xs p-2 font-bold"
-										>
-											<TooltipProvider delayDuration={300}>
-												<Tooltip>
-													<TooltipTrigger>
-														{pending ? (
-															<Loader2 className="animate-spin h-4 w-4"></Loader2>
-														) : (
-															<Lock className="h-4 w-4" />
-														)}
-													</TooltipTrigger>
-													<TooltipContent
-														className="p-0 pt-1 m-0 border-none outline-none font-mono bg-transparent text-xs font-extralight"
-														side="bottom"
-													>
-														<p>Make Private</p>
-													</TooltipContent>
-												</Tooltip>
-											</TooltipProvider>
-										</CardItem>
-									) : (
-										<CardItem
-											onClick={async () => {
-												setPending(true)
-												await publish(scenario.id)
-												router.refresh()
-												setPending(false)
-											}}
-											style={{ borderRadius: "1em" }}
-											className="mr-2 rounded-x bg-transparent hover:bg-white/20 text-xs p-2 font-bold"
-										>
-											<TooltipProvider delayDuration={300}>
-												<Tooltip>
-													<TooltipTrigger>
-														{pending ? (
-															<Loader2 className="animate-spin h-4 w-4"></Loader2>
-														) : (
-															<Globe className="h-4 w-4" />
-														)}
-													</TooltipTrigger>
-													<TooltipContent
-														className="p-0 pt-1 m-0 border-none outline-none font-mono bg-transparent text-xs font-extralight"
-														side="bottom"
-													>
-														<p>Publish</p>
-													</TooltipContent>
-												</Tooltip>
-											</TooltipProvider>
-										</CardItem>
-									)}
-								</>
-							)}
-						</div>
-					</CardItem>
-				</div>
-			</CardBody>
-		</CardContainer>
-	)
+    return (
+        <CardContainer className="inter-var h-[10rem] p-0 my-7">
+            <CardBody
+                className={`transition-all bg-gray-50 relative group/card shadow-2xl dark:bg-black/50 ${bordercolor[r]} ${shadowcolor[r]} hover:border-white w-auto h-auto max-md:h-auto my-auto sm:w-[25rem] max-w-[25rem] rounded-xl p-7 m-10 border flex flex-col`}
+            >
+                <CardItem
+                    href={`/story/${scenario.id}`}
+                    as="a"
+                    style={{ cursor: "pointer" }}
+                    translateZ="50"
+                    className="text-xl font-bold text-neutral-600 dark:text-white"
+                >
+                    {title}
+                </CardItem>
+                <CardItem
+                    as="p"
+                    translateZ="60"
+                    className="text-neutral-400 text-xs max-w-sm mt-2 dark:text-neutral-400/80 hover:dark:text-neutral-400/100"
+                >
+                    {truncateString(story!!, 180)}
+                </CardItem>
+                <CardItem
+                    as="p"
+                    translateZ={60}
+                    style={{
+                        backgroundColor: "rgba(0,0,0,0.700)",
+                        borderRadius: "1em",
+                        lineHeight: "1rem",
+                        overflowWrap: "break-word",
+                    }}
+                    className="text-neutral-600 p-2 transition-all duration-1000 ease-in-out font-mono text-[0.6rem] hover:text-[0.8rem] w-[100%] my-4 dark:text-neutral-500 hover:dark:text-neutral-300"
+                >
+                    Prompt: {prompt}
+                </CardItem>
+                <div className="flex justify-between items-center mt-auto">
+                    {finished == false && (
+                        <CardItem
+                            as={"a"}
+                            href={`/app/library/continue/${scenario.id}`}
+                            translateZ={74}
+                            style={{ borderRadius: "1em" }}
+                            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white bg-transparent hover:bg-white/20 "
+                        >
+                            Continue →
+                        </CardItem>
+                    )}
+                    <CardItem translateZ={70}>
+                        <div className="flex justify-end mt-auto">
+                            {finished == true && (
+                                <>
+                                    {published ? (
+                                        <CardItem
+                                            onClick={async () => {
+                                                setPending(true);
+                                                await unPublish(scenario.id);
+                                                router.refresh();
+                                                setPending(false);
+                                            }}
+                                            style={{ borderRadius: "1em" }}
+                                            className="mr-2 rounded-x bg-transparent hover:bg-white/20 text-xs p-2 font-bold"
+                                        >
+                                            <TooltipProvider
+                                                delayDuration={300}
+                                            >
+                                                <Tooltip>
+                                                    <TooltipTrigger>
+                                                        {pending ? (
+                                                            <Loader2 className="animate-spin h-4 w-4"></Loader2>
+                                                        ) : (
+                                                            <Lock className="h-4 w-4" />
+                                                        )}
+                                                    </TooltipTrigger>
+                                                    <TooltipContent
+                                                        className="p-0 pt-1 m-0 border-none outline-none font-mono bg-transparent text-xs font-extralight"
+                                                        side="bottom"
+                                                    >
+                                                        <p>Make Private</p>
+                                                    </TooltipContent>
+                                                </Tooltip>
+                                            </TooltipProvider>
+                                        </CardItem>
+                                    ) : (
+                                        <CardItem
+                                            onClick={async () => {
+                                                setPending(true);
+                                                await publish(scenario.id);
+                                                router.refresh();
+                                                setPending(false);
+                                            }}
+                                            style={{ borderRadius: "1em" }}
+                                            className="mr-2 rounded-x bg-transparent hover:bg-white/20 text-xs p-2 font-bold"
+                                        >
+                                            <TooltipProvider
+                                                delayDuration={300}
+                                            >
+                                                <Tooltip>
+                                                    <TooltipTrigger>
+                                                        {pending ? (
+                                                            <Loader2 className="animate-spin h-4 w-4"></Loader2>
+                                                        ) : (
+                                                            <Globe className="h-4 w-4" />
+                                                        )}
+                                                    </TooltipTrigger>
+                                                    <TooltipContent
+                                                        className="p-0 pt-1 m-0 border-none outline-none font-mono bg-transparent text-xs font-extralight"
+                                                        side="bottom"
+                                                    >
+                                                        <p>Publish</p>
+                                                    </TooltipContent>
+                                                </Tooltip>
+                                            </TooltipProvider>
+                                        </CardItem>
+                                    )}
+                                </>
+                            )}
+                        </div>
+                    </CardItem>
+                </div>
+            </CardBody>
+        </CardContainer>
+    );
 }
