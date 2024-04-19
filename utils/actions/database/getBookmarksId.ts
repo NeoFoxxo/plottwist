4099956f2433386752e44a96e2b2b607f9539db7
nidhish-nayak <nidhibelthangady@gmail.com>
2024-04-19 11:48:54 +0530

    Merging incoming changes

--- utils/actions/database/getBookmarksId.ts
+++ utils/actions/database/getBookmarksId.ts
@@ -1,15 +1,15 @@
 import { createClient } from "@/utils/supabase/server";
 
 export async function getBookmarksId(user_id: string | null) {
-    const supabase = createClient();
+  const supabase = createClient();
 
-    const { data: bookmarks, error: bookmarksError } = await supabase
-        .from("bookmarks")
-        .select('scenario_id')
-        .eq("user_id", user_id!!);
-    if (bookmarksError) throw Error("Bookmarks fetch failed!");
+  const { data: bookmarks, error: bookmarksError } = await supabase
+    .from("bookmarks")
+    .select("scenario_id")
+    .eq("user_id", user_id!!);
+  if (bookmarksError) throw Error("Bookmarks fetch failed!");
 
-    const bookmarkedStories = bookmarks.map((bookmark) => bookmark.scenario_id);
+  const bookmarkedStories = bookmarks.map((bookmark) => bookmark.scenario_id);
 
-    return bookmarkedStories;
+  return bookmarkedStories;
 }
