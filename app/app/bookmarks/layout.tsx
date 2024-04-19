3499865714ed0ecdc847211e23efe632ca6b6316
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-04-19 20:58:47 +0300

    fix dashboard layouts' responsiveness

--- app/app/bookmarks/layout.tsx
+++ app/app/bookmarks/layout.tsx
@@ -2,7 +2,7 @@ import { Sidebar } from "@/components/Sidebar";
 
 const BookmarksLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="w-full mt-10 flex justify-start items-start min-h-[84vh] pl-[70px]">
+        <div className="w-full mt-10 flex justify-start items-start min-h-[84vh] pl-[70px] max-lg:pl-0">
             <Sidebar />
             {children}
         </div>
