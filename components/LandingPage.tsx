0af3149434a332b45afe7a44f1f658b20dc32bfa
bonndevoff <12user12b12@gmail.com>
2024-04-19 17:09:36 -0300

    new landing page style

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,53 +1,79 @@
 "use client";
 
 import { motion } from "framer-motion";
-import Image from "next/image";
 import { HoverEffect } from "./ui/card-hover-effect";
-import { ContainerScroll } from "./ui/container-scroll-animation";
-import { HeroHighlight, Highlight } from "./ui/hero-highlight";
+import { HeroHighlight } from "./ui/hero-highlight";
+import { AnimatedTooltip } from "./ui/animated-tooltip";
+import { Button } from "./ui/button";
 
 export function LandingPage() {
+
+    const people = [
+        {
+            id: 1,
+            name: "NeoFox",
+            designation: "TypeScript Techie",
+            image:
+                "https://avatars.githubusercontent.com/u/104744179",
+        },
+        {
+            id: 2,
+            name: "xhyabunny",
+            designation: "React & AI Logics",
+            image:
+                "https://avatars.githubusercontent.com/u/106491722",
+        },
+        {
+            id: 3,
+            name: "Slaviik",
+            designation: "Tailwind Expert",
+            image:
+                "https://avatars.githubusercontent.com/u/106228555",
+        },
+        {
+            id: 4,
+            name: "Nidhish",
+            designation: "Database & BugFixer",
+            image:
+                "https://avatars.githubusercontent.com/u/76598208",
+        }
+    ];
+
     const projects = [
         {
-            title: "Stripe",
-            description:
-                "A technology company that builds economic infrastructure for the internet.",
-            link: "https://stripe.com",
+            title: "SmythOS",
+            description: "Empower your development with SmythOS. Integrate AI, APIs, and data sources easily. Simplify complexity, enhance control, and accelerate innovation.",
+            link: "https://smythos.com",
         },
         {
-            title: "Netflix",
-            description:
-                "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
-            link: "https://netflix.com",
+            title: "Supabase",
+            description: "Open source Firebase alternative. Start with Postgres, Authentication, instant APIs, Edge Functions, Realtime subscriptions, Storage, and Vector embeddings.",
+            link: "https://supabase.com",
         },
         {
-            title: "Google",
-            description:
-                "A multinational technology company that specializes in Internet-related services and products.",
-            link: "https://google.com",
+            title: "Next.js",
+            description: "The React framework for most server-rendered websites.",
+            link: "https://nextjs.org",
         },
         {
-            title: "Meta",
-            description:
-                "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
-            link: "https://meta.com",
+            title: "Vercel",
+            description: "Website host for Plot Twist",
+            link: "https://vercel.com",
         },
         {
-            title: "Amazon",
-            description:
-                "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
-            link: "https://amazon.com",
+            title: "React.ts",
+            description: "React with TypeScript for scalable and maintainable applications.",
+            link: "https://www.typescriptlang.org/docs/handbook/react.html",
         },
         {
-            title: "Microsoft",
-            description:
-                "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
-            link: "https://microsoft.com",
+            title: "Tailwind",
+            description: "A utility-first CSS framework for fast and responsive web development.",
+            link: "https://tailwindcss.com/",
         },
     ];
 
     return (
-        <div className="bg-[url('/gif4.gif')] bg-background bg-cover bg-fixed bg-no-repeat bg-center flex flex-col items-center justify-start w-full h-full py-10">
+        <div className="bg-[url('/gif4.gif')] bg-background bg-cover bg-fixed bg-no-repeat bg-center flex flex-col items-center justify-center text-center w-full h-full py-10">
             <HeroHighlight>
                 <motion.h1
                     initial={{
@@ -64,52 +90,97 @@ export function LandingPage() {
                     }}
                     className="max-w-6xl px-4 mx-auto text-4xl font-bold leading-relaxed text-center md:text-5xl lg:text-7xl text-secondary-foreground dark:text-slate-200 lg:leading-snug"
                 >
-                    With plot twist <br />{" "}
-                    <span className="italic font-thin">
-                        {" "}
-                        Nothing&apos;s real.
-                    </span>
-                    <br />{" "}
-                    <span className="font-mono font-medium ">
-                        Everything is far away.
-                    </span>{" "}
-                    <br />
-                    <span className="italic font-light ">Everything is</span>
-                    <br />
-                    <Highlight className="text-white dark:text-background">
-                        imaginary
-                    </Highlight>
+                    Plot Twist
                 </motion.h1>
             </HeroHighlight>
-            <div className="flex flex-col overflow-hidden">
-                <ContainerScroll
-                    titleComponent={
-                        <>
-                            <h1 className="text-4xl font-semibold">
-                                Unleash the power of <br />
-                                <span className="text-4xl md:text-[6rem] font-bold leading-none">
-                                    Stories
-                                </span>
-                            </h1>
-                        </>
-                    }
+            <HeroHighlight>
+                <motion.h2
+                    initial={{
+                        opacity: 0,
+                        y: 0,
+                    }}
+                    animate={{
+                        opacity: 1,
+                        y: [40, 0, 0],
+                    }}
+                    transition={{
+                        duration: 0.5,
+                        ease: [0.4, 0.0, 0.2, 1],
+                    }}
+                    className="mt-[-4rem]"
                 >
-                    <Image
-                        src={`https://www.projectmanager.com/wp-content/uploads/2022/03/Dashboard_Construction_Wide_Zoom-150.jpg`}
-                        alt="hero"
-                        height={720}
-                        width={1400}
-                        className="object-cover object-left-top h-full mx-auto rounded-2xl"
-                        draggable={false}
-                    />
-                </ContainerScroll>
-            </div>
-            <div className="max-w-5xl px-8 mx-auto">
-                <h2 className="pb-6 text-5xl font-bold text-center">
-                    Popular Topics
-                </h2>
-                <HoverEffect items={projects} />
-            </div>
+                    <div className="flex flex-col overflow-hidden pb-7">
+                        <p className="text-lg pt-4">Create stories and choose your own adventure powered by <a className="text-green-300 hover:underline" href="https://smythos.com">SmythOS</a> and <a className="text-green-300 hover:underline" href="https://supabase.com/">Supabase</a></p>
+                        <p className="text-lg text-center pt-2">Made for the <a className="text-green-300 hover:underline" href="https://supabase.com/blog/supabase-oss-hackathon">#hackathon</a> contest</p>
+                    </div>
+                </motion.h2>
+            </HeroHighlight>
+            <HeroHighlight>
+                <motion.h2
+                    initial={{
+                        opacity: 0,
+                        y: 0,
+                    }}
+                    animate={{
+                        opacity: 1,
+                        y: [40, 0, 0],
+                    }}
+                    transition={{
+                        duration: 0.5,
+                        ease: [0.4, 0.0, 0.2, 1],
+                    }}
+                    className="mt-[-7rem]"
+                >
+                    <div className="justify-center pb-20">
+                        <h2 className="pb-2">Meet our team!</h2>
+                        <div className="ml-[-1rem] flex flex-row">
+                            <AnimatedTooltip items={people} />
+                        </div>
+                    </div>
+                </motion.h2>
+            </HeroHighlight>
+            <HeroHighlight>
+                <motion.h2
+                    initial={{
+                        opacity: 0,
+                        y: 0,
+                    }}
+                    animate={{
+                        opacity: 1,
+                        y: [40, 0, 0],
+                    }}
+                    transition={{
+                        duration: 0.5,
+                        ease: [0.4, 0.0, 0.2, 1],
+                    }}
+                    className="mt-[-7rem]"
+                >
+                    <Button onClick={() => { window.location.assign('./login') }} className="w-40 h-10 text-xl" variant='default'>Try it out →</Button>
+                </motion.h2>
+            </HeroHighlight>
+            <HeroHighlight>
+                <motion.footer
+                    initial={{
+                        opacity: 0,
+                        y: 20,
+                    }}
+                    animate={{
+                        opacity: 1,
+                        y: [20, -5, 0],
+                    }}
+                    transition={{
+                        duration: 0.8,
+                        ease: [0.5, 0.0, 0.4, 1],
+                    }}
+                >
+                    <div className="max-w-5xl px-8 mx-auto">
+                        <h2 className="pb-6 text-5xl font-bold text-center">
+                            Our tools
+                        </h2>
+                        <HoverEffect items={projects} />
+                    </div>
+                </motion.footer>
+            </HeroHighlight>
         </div>
     );
 }
