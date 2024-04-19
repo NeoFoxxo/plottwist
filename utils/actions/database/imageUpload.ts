da3bd5c64516afb81c9b2ee8e649495020a085b3
nidhish-nayak <nidhibelthangady@gmail.com>
2024-04-19 23:36:56 +0530

    Added image compression for better scaling

--- utils/actions/database/imageUpload.ts
+++ utils/actions/database/imageUpload.ts
@@ -1,19 +1,35 @@
 import { createClient } from "@/utils/supabase/client";
+import imageCompression from "browser-image-compression";
+
+async function compressImage(file: File) {
+  const options = {
+    maxSizeMB: 1,
+    useWebWorker: true,
+  };
+  try {
+    const compressedFile = await imageCompression(file, options);
+    return compressedFile;
+  } catch (error) {
+    console.log(error);
+    throw Error("Compression failed!");
+  }
+}
 
 export default async function imageUpload(file: File) {
-    const supabase = createClient();
+  const supabase = createClient();
 
-    const fileNamePrefix: string = Date.now().toString();
-    const fileName: string = fileNamePrefix + file.name;
+  const fileNamePrefix: string = Date.now().toString();
+  const fileName: string = fileNamePrefix + file.name;
 
-    const pathPrefix = process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL;
+  const pathPrefix = process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL;
 
-    if (!pathPrefix) throw Error(".env for Storage URL not found!");
+  if (!pathPrefix) throw Error(".env for Storage URL not found!");
 
-    const { data, error } = await supabase.storage
-        .from("avatars")
-        .upload(fileName, file);
+  const compressedFile = await compressImage(file);
+  const { data, error } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, compressedFile);
 
-    if (error) return false;
-    return pathPrefix + "/" + data.path;
+  if (error) return false;
+  return pathPrefix + "/" + data.path;
 }
