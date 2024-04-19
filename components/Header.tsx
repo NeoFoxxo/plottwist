223ca9c48442520d6e27e692d45c48142bc22370
bonndevoff <12user12b12@gmail.com>
2024-04-19 03:28:24 -0300

    Changed dropdown menu to username and profile page works properly now

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,11 @@ import AuthButton from "@/components/AuthButton"
 import ProfileDropdown from "@/components/ProfileDropdown"
 
 export default function Header(
-	{ email,
-		userId
-	}: { email: string | undefined, userId: string | undefined }) {
+	{
+		email,
+		userId,
+		username
+	}: { email: string | undefined, username: string | any, userId: string | undefined }) {
 	return (
 		<nav className="fixed z-30 flex items-center justify-center w-full h-16 border-b bg-zinc-950 border-secondary">
 			<div className="flex items-center justify-between w-full p-3 text-sm max-w-7xl">
@@ -21,7 +23,7 @@ export default function Header(
 					</div>
 				</Link>
 				<div className="flex gap-5">
-					{email && userId ? <ProfileDropdown email={email} userId={userId} /> : <AuthButton />}
+					{email && userId ? <ProfileDropdown email={email} username={username} /> : <AuthButton />}
 				</div>
 			</div>
 		</nav>
