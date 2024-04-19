916ed68fdc1bc73c18a523abd1535d6f49d1800a
NeoFoxxo <hamish@cits.co.nz>
2024-04-19 21:24:21 +1000

    Prevent users from continue other users' stories

--- app/app/library/continue/[id]/page.tsx
+++ app/app/library/continue/[id]/page.tsx
@@ -1,26 +1,32 @@
-import ContinueStory from "@/components/ContinueStory";
-import { getStory } from "@/utils/actions/database/getStory";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
+import NotFound from "@/app/not-found"
+import ContinueStory from "@/components/ContinueStory"
+import { getStory, getStoryReturnType } from "@/utils/actions/database/getStory"
+import { createClient } from "@/utils/supabase/server"
+import { redirect } from "next/navigation"
 
 export default async function Create({ params }: { params: { id: string } }) {
-    const scenario = await getStory(params.id);
-    const supabase = createClient();
+	const supabase = createClient()
 
-    const {
-        data: { user },
-    } = await supabase.auth.getUser();
+	const {
+		data: { user },
+	} = await supabase.auth.getUser()
 
-    const user_id = user?.id;
-    if (!user_id) redirect("/login");
+	const user_id = user?.id
+	if (!user_id) redirect("/login")
 
-    return (
-        <main className="flex flex-col flex-1 my-4 md:p-10 justify-start items-center w-full md:w-fit">
-            {scenario ? (
-                <ContinueStory scenarioData={scenario} />
-            ) : (
-                <div>No story data found!</div>
-            )}
-        </main>
-    );
+	let scenario
+	try {
+		scenario = await getStory(params.id)
+	} catch (error) {
+		return <NotFound />
+	}
+
+	// if user did not create the story 404 them
+	if (scenario?.user_id != user_id) return <NotFound />
+
+	return (
+		<main className="flex flex-col flex-1 my-4 md:p-10 justify-start items-center w-full md:w-fit">
+			<ContinueStory scenarioData={scenario!!} />
+		</main>
+	)
 }
