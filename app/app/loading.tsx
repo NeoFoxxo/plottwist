f877db93321780faf37daf4d609c5e133e33a3b1
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-04-20 21:40:45 +0300

    add loader

--- app/app/loading.tsx
+++ app/app/loading.tsx
@@ -1,3 +1,11 @@
 export default function Loading() {
-	return <>Loading....</>
+	return (
+		<div className="flex items-center h-screen pb-20">
+			<div className="relative inline-flex">
+				<div className="w-24 h-24 rounded-full bg-primary"></div>
+				<div className="absolute top-0 left-0 w-24 h-24 rounded-full bg-primary animate-ping"></div>
+				<div className="absolute top-0 left-0 w-24 h-24 rounded-full bg-primary animate-pulse"></div>
+			</div>
+		</div>
+	)
 }
