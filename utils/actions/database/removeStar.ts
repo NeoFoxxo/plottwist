f682f860276f0a57805c590eeb625b7b119bfeab
NeoFoxxo <hamish@cits.co.nz>
2024-04-20 13:42:12 +1000

    Full star functionality with stored procedures

--- utils/actions/database/removeStar.ts
+++ utils/actions/database/removeStar.ts
@@ -1,18 +1,15 @@
 import { createClient } from "@/utils/supabase/client"
 
-export async function addStar({
+export async function removeStar({
 	current_user_id,
 	author_id,
 }: {
 	current_user_id: string | undefined
 	author_id: string | undefined
 }) {
-	console.log(author_id)
-	console.log("hi")
-	console.log(current_user_id)
 	const supabase = createClient()
 
-	const { error } = await supabase.rpc("increment_star", {
+	const { error } = await supabase.rpc("remove_star", {
 		current_user_id,
 		author_id,
 	})
