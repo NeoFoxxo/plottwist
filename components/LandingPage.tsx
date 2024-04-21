b1c2eacad474d61eefcf02699d493cb0852802e7
NeoFoxxo <hamish@cits.co.nz>
2024-04-21 23:26:04 +1000

    Updated landing page to revert header & changed api error messages

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,79 +1,78 @@
-"use client";
+"use client"
 
-import { motion } from "framer-motion";
-import { HoverEffect } from "./ui/card-hover-effect";
-import { HeroHighlight } from "./ui/hero-highlight";
-import { AnimatedTooltip } from "./ui/animated-tooltip";
-import { Button } from "./ui/button";
+import { motion } from "framer-motion"
+import { HoverEffect } from "./ui/card-hover-effect"
+import { HeroHighlight } from "./ui/hero-highlight"
+import { AnimatedTooltip } from "./ui/animated-tooltip"
+import Link from "next/link"
 
 export function LandingPage() {
-
 	const people = [
 		{
 			id: 1,
 			name: "NeoFox",
 			designation: "Full Stack Dev",
-			image:
-				"https://avatars.githubusercontent.com/u/104744179",
+			image: "https://avatars.githubusercontent.com/u/104744179",
 		},
 		{
 			id: 2,
 			name: "xhyabunny",
 			designation: "ReactTS & AI Logics",
-			image:
-				"https://avatars.githubusercontent.com/u/106491722",
+			image: "https://avatars.githubusercontent.com/u/106491722",
 		},
 		{
 			id: 3,
 			name: "Slaviik",
 			designation: "Tailwind Expert",
-			image:
-				"https://avatars.githubusercontent.com/u/106228555",
+			image: "https://avatars.githubusercontent.com/u/106228555",
 		},
 		{
 			id: 4,
 			name: "Nidhish",
 			designation: "Database & BugFixer",
-			image:
-				"https://avatars.githubusercontent.com/u/76598208",
-		}
-	];
+			image: "https://avatars.githubusercontent.com/u/76598208",
+		},
+	]
 
 	const projects = [
 		{
 			title: "SmythOS",
-			description: "Empower your development with SmythOS. Integrate AI, APIs, and data sources easily. Simplify complexity, enhance control, and accelerate innovation.",
+			description:
+				"No-code tool for creating APIs that can intergrate with AI and other data sources.",
 			link: "https://smythos.com",
 		},
 		{
 			title: "Supabase",
-			description: "Open source Firebase alternative. Start with Postgres, Authentication, instant APIs, Edge Functions, Realtime subscriptions, Storage, and Vector embeddings.",
+			description:
+				"Open source Firebase alternative. Used for the Postgres database, authentication, and Bucket storage.",
 			link: "https://supabase.com",
 		},
 		{
 			title: "Next.js",
-			description: "The React framework for most server-rendered websites.",
+			description: "React framework for fullstack functionality",
 			link: "https://nextjs.org",
 		},
 		{
 			title: "Vercel",
-			description: "Website host for Plot Twist",
+			description: "Serverless hosting for Plot Twist ",
 			link: "https://vercel.com",
 		},
 		{
 			title: "React.ts",
-			description: "React with TypeScript for scalable and maintainable applications.",
-			link: "https://www.typescriptlang.org/docs/handbook/react.html",
+			description:
+				"React with TypeScript for scalable and maintainable applications.",
+			link: "https://react.dev/",
 		},
 		{
-			title: "Tailwind",
-			description: "A utility-first CSS framework for fast and responsive web development.",
+			title: "Tailwind CSS",
+			description:
+				"A utility-first CSS framework for fast and responsive web development.",
 			link: "https://tailwindcss.com/",
 		},
-	];
+	]
 
 	return (
-		<div className="bg-[url('/gif4.gif')] bg-background bg-cover bg-fixed bg-no-repeat bg-center flex flex-col items-center justify-center text-center w-full h-full py-10">
+		<div className="bg-[url('/gif4.gif')] bg-cover bg-fixed bg-no-repeat bg-center flex flex-col items-center justify-center text-center w-full h-full py-20">
 			<HeroHighlight>
 				<motion.h1
 					initial={{
@@ -88,9 +87,9 @@ export function LandingPage() {
 						duration: 0.5,
 						ease: [0.4, 0.0, 0.2, 1],
 					}}
-					className="max-w-6xl px-4 mx-auto text-4xl font-bold leading-relaxed text-center md:text-5xl lg:text-7xl text-secondary-foreground dark:text-slate-200 lg:leading-snug"
+					className="max-w-4xl px-4 mx-auto text-4xl font-bold leading-relaxed text-center md:text-5xl lg:text-7xl text-secondary-foreground dark:text-slate-200 lg:leading-snug"
 				>
-					Plot Twist
+					Create Interactive Stories
 				</motion.h1>
 			</HeroHighlight>
 			<HeroHighlight>
@@ -110,13 +109,19 @@ export function LandingPage() {
 					className="mt-[-4rem]"
 				>
 					<div className="flex flex-col overflow-hidden pb-7">
-						<p className="text-lg pt-4">Create stories and choose your own adventure powered by <a className="text-green-300 hover:underline" href="https://smythos.com">SmythOS</a> and <a className="text-green-300 hover:underline" href="https://supabase.com/">Supabase</a></p>
-						<p className="text-lg text-center pt-2">Made for the <a className="text-green-300 hover:underline" href="https://supabase.com/blog/supabase-oss-hackathon">#hackathon</a> contest</p>
+						<h2 className="pt-0 text-lg md:text-xl">
+							Generate interactive stories where{" "}
+							<b>your choices shape the narrative</b>.
+						</h2>
+						<h3 className="pt-2 text-lg md:text-xl">
+							Start with a prompt, and let our custom AI generate the story and
+							choices you can make
+						</h3>
 					</div>
 				</motion.h2>
 			</HeroHighlight>
 			<HeroHighlight>
-				<motion.h2
+				<motion.h4
 					initial={{
 						opacity: 0,
 						y: 0,
@@ -129,36 +134,20 @@ export function LandingPage() {
 						duration: 0.5,
 						ease: [0.4, 0.0, 0.2, 1],
 					}}
-					className="mt-[-7rem]"
+					className="mt-[-5rem]"
 				>
-					<div className="justify-center pb-20">
-						<h2 className="pb-2">Meet our team!</h2>
-						<div className="ml-[-1rem] flex flex-row">
-							<AnimatedTooltip items={people} />
-						</div>
-					</div>
-				</motion.h2>
+					{" "}
+					<Link href="/app">
+						<button className="p-[3px] relative">
+							<div className="absolute inset-0 rounded-lg bg-gray-300" />
+							<div className="px-8 py-2  bg-black rounded-[6px] relative group transition duration-200 text-white">
+								Create your own interactive story →
+							</div>
+						</button>
+					</Link>
+				</motion.h4>
 			</HeroHighlight>
-			<HeroHighlight>
-				<motion.h2
-					initial={{
-						opacity: 0,
-						y: 0,
-					}}
-					animate={{
-						opacity: 1,
-						y: [40, 0, 0],
-					}}
-					transition={{
-						duration: 0.5,
-						ease: [0.4, 0.0, 0.2, 1],
-					}}
-					className="mt-[-7rem]"
-				>
-					<Button onClick={() => { window.location.assign('./app') }} className="w-40 h-10 text-xl" variant='default'>Try it out →</Button>
-				</motion.h2>
-			</HeroHighlight>
-			<HeroHighlight>
+			<HeroHighlight className="pt-40">
 				<motion.footer
 					initial={{
 						opacity: 0,
@@ -174,13 +163,64 @@ export function LandingPage() {
 					}}
 				>
 					<div className="max-w-5xl px-8 mx-auto">
-						<h2 className="pb-6 text-5xl font-bold text-center">
-							Our tools
-						</h2>
+						<h2 className="pb-6 text-5xl font-bold text-center">Our tools</h2>
+						<div className="pb-8">
+							<h4 className="pt-2 text-lg text-center">
+								App made for the Supabase{" "}
+								<a
+									className="text-green-300 hover:underline"
+									href="https://supabase.com/blog/supabase-oss-hackathon"
+								>
+									#hackathon
+								</a>{" "}
+								contest
+							</h4>
+							<h5>
+								Powered by{" "}
+								<a
+									className="text-green-300 hover:underline"
+									href="https://smythos.com"
+								>
+									SmythOS
+								</a>{" "}
+								and{" "}
+								<a
+									className="text-green-300 hover:underline"
+									href="https://supabase.com/"
+								>
+									Supabase
+								</a>
+							</h5>
+						</div>
 						<HoverEffect items={projects} />
 					</div>
 				</motion.footer>
 			</HeroHighlight>
+			<HeroHighlight>
+				<motion.h2
+					initial={{
+						opacity: 0,
+						y: 0,
+					}}
+					animate={{
+						opacity: 1,
+						y: [40, 0, 0],
+					}}
+					transition={{
+						duration: 0.5,
+						ease: [0.4, 0.0, 0.2, 1],
+					}}
+					className="mt-[-5rem]"
+				>
+					{" "}
+					<div className="justify-center pt-2">
+						<h2 className="pb-2">Meet our team!</h2>
+						<div className="ml-[-1rem] flex flex-row">
+							<AnimatedTooltip items={people} />
+						</div>
+					</div>
+				</motion.h2>
+			</HeroHighlight>
 		</div>
-	);
+	)
 }
