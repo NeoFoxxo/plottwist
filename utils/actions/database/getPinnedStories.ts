667fb0353439afe8333cf4f864206eefe254bcfd
nidhish-nayak <nidhibelthangady@gmail.com>
2024-04-21 11:23:20 +0530

    Updating some hydration errors, Fixed sorting for my-library

--- utils/actions/database/getPinnedStories.ts
+++ utils/actions/database/getPinnedStories.ts
@@ -1,14 +1,24 @@
 import { createClient } from "@/utils/supabase/server";
 
 export default async function getPinnedStories(user_id: string) {
-    const supabase = createClient();
+  const supabase = createClient();
 
-    let { data: userStories, error } = await supabase
-        .from("scenarios")
-        .select("*")
-        .order("pinned", { ascending: false })
-        .eq("user_id", user_id);
+  let { data: userStories, error } = await supabase
+    .from("scenarios")
+    .select("*")
+    .order("pinned", { ascending: false })
+    .eq("user_id", user_id);
 
-    if (error) throw new Error("Error when fetching user's stories");
-    return userStories;
+  const sortedStories = userStories?.sort((a, b) => {
+    if (a.pinned === b.pinned) {
+      return (
+        new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+      );
+    } else {
+      return a.pinned ? -1 : 1;
+    }
+  });
+
+  if (error) throw new Error("Error when fetching user's stories");
+  return sortedStories;
 }
