939228851a4e7b32157b63bb3399467b25727102
nidhish-nayak <nidhibelthangady@gmail.com>
2024-04-22 13:08:26 +0530

    Fixed navbar for no user - limit card on mobile

--- components/CardForLimit.tsx
+++ components/CardForLimit.tsx
@@ -1,34 +1,34 @@
-"use client"
-import React, { useEffect } from 'react';
-import {
-    Card,
-    CardContent,
-    CardHeader,
-} from "@/components/ui/card";
-import { useRouter } from 'next/navigation';
+"use client";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function CardForLimit({ stories }: { stories: number }) {
+  const router = useRouter();
 
-    const router = useRouter()
+  useEffect(() => {
+    router.refresh();
+  }, [stories]);
 
-    useEffect(() => { router.refresh() }, [stories])
-
-    return (
-        <Card className="mb-4 border-none overflow-hidden bg-black/50">
-            <div className="hidden opacity-0 max-lg:block max-lg:opacity-100 lg:group-hover:block lg:group-hover:opacity-100">
-                <CardHeader className="text-2xl font-bold">
-                    <h2>{stories}/10 Stories Generated</h2>
-                </CardHeader>
-                <CardContent className="hidden opacity-0 max-lg:block max-lg:opacity-100 lg:group-hover:block lg:group-hover:opacity-100">
-                    <p className="text-xs">This limit will be reset at 12AM EST.</p>
-                </CardContent>
-            </div>
-            <div className="block opacity-100 max-lg:block max-lg:opacity-100 lg:group-hover:hidden lg:group-hover:opacity-0 text-center mx-auto">
-                {stories}/10
-            </div>
-            <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: stories?.toString() + '0%' }}></div>
-            </div>
-        </Card>
-    );
+  return (
+    <Card className="mb-4 border-none overflow-hidden bg-black/50">
+      <div className="hidden opacity-0 max-lg:block max-lg:opacity-100 lg:group-hover:block lg:group-hover:opacity-100">
+        <CardHeader className="text-2xl font-bold">
+          <h2>{stories}/10 Stories Generated</h2>
+        </CardHeader>
+        <CardContent className="hidden opacity-0 max-lg:block max-lg:opacity-100 lg:group-hover:block lg:group-hover:opacity-100">
+          <p className="text-xs">This limit will be reset at 12AM EST.</p>
+        </CardContent>
+      </div>
+      <div className="block opacity-100 max-lg:block max-lg:opacity-100 lg:group-hover:hidden lg:group-hover:opacity-0 text-center mx-auto">
+        {stories}/10
+      </div>
+      <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+        <div
+          className="bg-blue-600 h-2.5 rounded-full"
+          style={{ width: stories?.toString() + "0%" }}
+        ></div>
+      </div>
+    </Card>
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,74 +1,80 @@
-import Link from "next/link"
+import Link from "next/link";
 
-import AuthButton from "@/components/AuthButton"
-import CardForLimit from "@/components/CardForLimit"
-import ProfileDropdown from "@/components/ProfileDropdown"
+import AuthButton from "@/components/AuthButton";
+import CardForLimit from "@/components/CardForLimit";
+import ProfileDropdown from "@/components/ProfileDropdown";
 import {
-	Sheet,
-	SheetClose,
-	SheetContent,
-	SheetTrigger,
-} from "@/components/ui/sheet"
-import { cn } from "@/lib/utils"
-import { getMaxStories } from "@/utils/actions/database/getMaxStories"
-import { AlignRight } from "lucide-react"
-import { links } from "./Sidebar"
-import { Button, buttonVariants } from "./ui/button"
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { cn } from "@/lib/utils";
+import { getMaxStories } from "@/utils/actions/database/getMaxStories";
+import { createClient } from "@/utils/supabase/server";
+import { AlignRight } from "lucide-react";
+import { links } from "./Sidebar";
+import { Button, buttonVariants } from "./ui/button";
 
 export default async function Header({
-	email,
-	userId,
-	username,
+  email,
+  userId,
+  username,
 }: {
-	email: string | undefined
-	username: string | any
-	userId: string | undefined
+  email: string | undefined;
+  username: string | any;
+  userId: string | undefined;
 }) {
-	const storiesData = await getMaxStories()
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-	return (
-		<nav className="opacity-95 backdrop-blur-sm fixed z-30 flex items-center justify-center w-full h-16 border-b bg-zinc-950 border-secondary">
-			<div className="flex items-center justify-between w-full px-[5vw] text-sm">
-				<Link href="/app">
-					<div className="font-semibold text-md">
-						<img src="/icons/logo.png" className="w-12 h-12" />
-					</div>
-				</Link>
-				<div className="flex gap-3">
-					<Sheet>
-						<SheetTrigger asChild>
-							<Button variant="ghost" className="hidden max-lg:block">
-								<AlignRight />
-							</Button>
-						</SheetTrigger>
-						<SheetContent className="flex flex-col justify-between h-full pb-40">
-							<nav className="flex flex-col mt-4 gap-9">
-								{links.map((link, index) => (
-									<SheetClose asChild>
-										<Link
-											key={index}
-											href={link.href}
-											className={cn(
-												buttonVariants({ variant: "ghost" }),
-												"flex justify-start items-center gap-3 px-3.5 py-2.5 hover:bg-secondary"
-											)}
-										>
-											<span className="block">{link.icon}</span>
-											<span>{link.text}</span>
-										</Link>
-									</SheetClose>
-								))}
-							</nav>
-							<CardForLimit stories={storiesData!!.storyCount!!} />
-						</SheetContent>
-					</Sheet>
-					{email && userId ? (
-						<ProfileDropdown email={email} username={username} />
-					) : (
-						<AuthButton />
-					)}
-				</div>
-			</div>
-		</nav>
-	)
+  const storiesData = await getMaxStories();
+
+  return (
+    <nav className="opacity-95 backdrop-blur-sm fixed z-30 flex items-center justify-center w-full h-16 border-b bg-zinc-950 border-secondary">
+      <div className="flex items-center justify-between w-full px-[5vw] text-sm">
+        <Link href="/app">
+          <div className="font-semibold text-md">
+            <img src="/icons/logo.png" className="w-12 h-12" />
+          </div>
+        </Link>
+        <div className="flex gap-3">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="ghost" className="hidden max-lg:block">
+                <AlignRight />
+              </Button>
+            </SheetTrigger>
+            <SheetContent className="flex flex-col justify-between h-full pb-40">
+              <nav className="flex flex-col mt-4 gap-9">
+                {links.map((link, index) => (
+                  <SheetClose asChild>
+                    <Link
+                      key={index}
+                      href={link.href}
+                      className={cn(
+                        buttonVariants({ variant: "ghost" }),
+                        "flex justify-start items-center gap-3 px-3.5 py-2.5 hover:bg-secondary"
+                      )}
+                    >
+                      <span className="block">{link.icon}</span>
+                      <span>{link.text}</span>
+                    </Link>
+                  </SheetClose>
+                ))}
+              </nav>
+              {user && <CardForLimit stories={storiesData!!.storyCount!!} />}
+            </SheetContent>
+          </Sheet>
+          {email && userId ? (
+            <ProfileDropdown email={email} username={username} />
+          ) : (
+            <AuthButton />
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
